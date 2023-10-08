febdf00804532f5b49d2c4a33db0d80671f16748
Chellapilla S V Bharadwaj <104909144+chellapillasvbharadwaj@users.noreply.github.com>
2023-10-08 23:58:37 +0530

    Added a-star3.js

---
+++ a-star3.js
@@ -0,0 +1,183 @@
+function removeFromArray(arr,elt){
+    for(var i=arr.length-1;i>=0;i--){
+        if(arr[i]==elt){
+            arr.splice(i,1);
+        }
+    }
+}
+function heuristic(a,b){
+    var d=dist(a.i,a.j,b.i,b.j);
+    // var d=abs(a.i-b.i)+abs(a.j-b.j);
+    return d;
+}
+var rows=localStorage.getItem('row');
+var cols=localStorage.getItem('col');
+var grid=new Array(cols);
+var openSet=[];
+var closedSet=[];
+var start;
+var end;
+var w;
+var h;
+var path=[];
+function Spot(i,j){
+    this.i=i;
+    this.j=j;
+    this.f=0;
+    this.g=0;
+    this.h=0;
+    this.neighbors=[];
+    this.previous=undefined;
+    this.wall=false;
+    var obst= localStorage.getItem('obs');
+    if(random(1)<(obst/10)){
+        this.wall=true;
+    }
+    this.show=function(col){
+        fill(col);
+        if(this.wall){
+            fill(0);
+        }
+        noStroke();
+        rect(this.i*w,this.j*h,w-1,h-1);
+    }
+    this.addNeighbors=function(grid){
+        var i=this.i;
+        var j=this.j;
+        if(i<cols-1){
+            this.neighbors.push(grid[i+1][j]);
+        }
+        if(i>0){
+            this.neighbors.push(grid[i-1][j]);
+        }
+        if(j<rows-1){
+            this.neighbors.push(grid[i][j+1]);
+        }
+        if(j>0){
+            this.neighbors.push(grid[i][j-1]);
+        }
+        if(i>0 && j>0){
+            this.neighbors.push(grid[i-1][j-1]);
+        }
+        if(i<cols-1 && j>0){
+            this.neighbors.push(grid[i+1][j-1]);
+        }
+        if(i>0 && j<rows-1){
+            this.neighbors.push(grid[i-1][j+1]);
+        }
+        if(i<cols-1 && j<rows-1){
+            this.neighbors.push(grid[i+1][j+1]);
+        }
+    }
+}
+function setup(){
+    let canvas=createCanvas(400,400);
+    canvas.parent('canvasContainer');
+    console.log('A*');
+    w=width/cols;
+    h=height/rows;
+    for(var i=0;i<cols;i++){
+        grid[i]=new Array(rows);
+    }
+    for(var i=0;i<cols;i++){
+        for(var j=0;j<rows;j++){
+            grid[i][j]=new Spot(i,j);
+        }
+    }
+    for(var i=0;i<cols;i++){
+        for(var j=0;j<rows;j++){
+            grid[i][j].addNeighbors(grid);
+        }
+    }
+    var s_st=localStorage.getItem('s_start');
+    var s_en=localStorage.getItem('s_end');
+    start=grid[s_en][s_st];
+    var st=localStorage.getItem('start');
+    var en=localStorage.getItem('end');
+    end=grid[en][st];
+    start.wall=false;
+    end.wall=false;
+    openSet.push(start);
+    console.log(grid);
+}
+function draw(){
+    if(openSet.length>0){
+         var winner=0;
+         for(var i=0;i<openSet.length;i++){
+            if(openSet[i].f<openSet[winner].f){
+                winner=i;
+            }
+         }
+         var current=openSet[winner];
+         if(current===end){
+            noLoop();
+            var result=document.getElementById('result');
+            console.log("DONE!");
+            result.style.color='darkblue';
+            result.style.fontWeight='bold';
+            result.style.fontSize='larger';
+            result.innerText='Done, optimal path found!';
+         }
+         removeFromArray(openSet,current);
+         closedSet.push(current);
+         var neighbors=current.neighbors;
+         for(var i=0;i<neighbors.length;i++){
+            var neighbor=neighbors[i];
+            if(!closedSet.includes(neighbor)&& !neighbor.wall){
+                var tempG=current.g+1;
+                var newPath=false;
+                if(openSet.includes(neighbor)){
+                    if(tempG<neighbor.G){
+                        neighbor.G=tempG;
+                        newPath=true;
+                    }
+                }else{
+                    neighbor.g=tempG;
+                    newPath=true;
+                    openSet.push(neighbor);
+                }
+                if(newPath){
+                    neighbor.h=heuristic(neighbor,end);
+                    neighbor.f=neighbor.g+neighbor.h;
+                    neighbor.previous=current;
+                }
+            }  
+        } 
+
+    }else{
+        console.log("No solution");
+        var result=document.getElementById('result');
+        result.style.color='darkblue';
+        result.innerText='No solution';
+        noLoop();
+        return;
+    }
+    background(0);
+    for(var i=0;i<cols;i++){
+        for(var j=0;j<rows;j++){
+            grid[i][j].show(color(255));
+        }
+    }
+    for(var i=0;i<closedSet.length;i++){
+        closedSet[i].show(color(255,0,0));
+    }
+    for(var i=0;i<openSet.length;i++){
+        openSet[i].show(color(0,255,0));
+    }
+   
+    path=[];
+    var temp=current;
+    path.push(temp);
+    while(temp.previous){
+        path.push(temp.previous);
+        temp=temp.previous;
+    }    
+    
+    for(var i=0;i<path.length;i++){
+        path[i].show(color(0,0,255));
+    }
+    stroke(0); 
+    strokeWeight(5);
+    noFill();
+    rect(0, 0, width, height); 
+}
